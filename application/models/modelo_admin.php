f399ee7d8b17c76e9312e5774dee3906234a2ed7
Eduardo Linares <eduardo.cangry9@gmail.com>
2014-10-03 10:26:19 -0600

    Actualizacion

---
+++ application/models/modelo_admin.php
@@ -0,0 +1,78 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class modelo_admin extends CI_Model {
+
+    function __construct()
+    {        
+        parent::__construct();
+    }
+
+    /*GENERALES*/
+
+    function guardar_item($datos, $tabla){
+        return $this->db->insert($tabla, $datos);
+    }
+
+    function act_item($datos, $id, $campo, $tabla){
+        $this->db->where($campo, $id);
+        return $this->db->update($tabla, $datos);
+    }
+
+    /*CARGOS*/
+
+    function obt_cargo($id){
+        return $this->db->get_where('cargo', array('cod_cargo'=>$id))->row_array();
+    }
+
+    function obt_cargos(){
+        return $this->db->get('cargo')->result_array();
+    }
+
+    /*CLINICAS*/
+
+    function obt_clinica($id){
+        return $this->db->get_where('clinica', array('cod_clinica'=>$id))->row_array();
+    }
+
+    function obt_clinicas(){
+        return $this->db->get('clinica')->result_array();
+    }
+
+    /*CONSULTORIOS*/
+
+    function obt_consultorio($id){
+        $datos = $this->db->query('SELECT *
+                                   FROM consultorio
+                                   WHERE cod_consultorio=?;', $id)->row_array();
+        return $datos;
+    }
+
+    function obt_consultorios(){
+        $datos = $this->db->query('SELECT co.*, cl.nombre AS nombre_clinica
+                                   FROM consultorio co, clinica cl
+                                   WHERE co.cod_clinica=cl.cod_clinica;')->result_array();
+        return $datos;
+    }
+
+    /*DEPARTAMENTOS*/
+
+    function obt_departamento($id){
+        return $this->db->get_where('departamento', array('cod_departamento'=>$id))->row_array();
+    }
+
+    function obt_departamentos(){
+        return $this->db->get('departamento')->result_array();
+    }
+
+    /*ESPECIALIDADES*/
+
+    function obt_especialidad($id){
+        return $this->db->get_where('especialidad', array('cod_especialidad'=>$id))->row_array();
+    }
+
+    function obt_especialidades(){
+        return $this->db->get('especialidad')->result_array();
+    }
+}
+
+?>
---
+++ application/views/lista_departamentos.php
@@ -0,0 +1,71 @@
+<!--LLAMAMOS EL ENCABEZADO-->
+<?php
+$data['titulo'] = $titulo;
+$this->load->view('administrador/encabezado', $data);
+?>
+<div class="content">
+        <div class="header">
+            
+            <h1 class="page-title">Departamentos</h1>
+                    <ul class="breadcrumb">
+            <li><a href="<?=base_url()?>inicio">Mantenimientos</a> </li>
+            <li class="active">Departamentos</li>
+        </ul>
+
+        </div>
+        <div class="main-content">
+
+        <?php
+        if($this->session->userdata('mensaje')){
+        ?>
+        <div class="alert alert-success">
+            <button type="button" class="close" data-dismiss="alert">×</button>
+             <?php   
+              echo $this->session->userdata('mensaje');             
+             ?>
+        </div>
+        <?php
+        }
+        $this->session->unset_userdata('mensaje');
+        ?>
+            
+<div class="btn-toolbar list-toolbar">
+    <a href="<?=base_url()?>agregar_departamento" class="btn btn-primary"><i class="fa fa-plus"></i> Nuevo Departamento</a>
+  <div class="btn-group">
+  </div>
+</div>
+<?php
+if(empty($departamentos)){
+  echo "No se encontraron registros.";
+}else{  
+?>
+<table class="table">
+  <thead>
+    <tr>
+      <th>Nombre</th>
+      <th style="width: 3.5em;"></th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+      foreach ($departamentos as $departamento) {
+        ?>
+        <tr>
+          <td><?=$departamento['nombre']?></td>
+          <td>
+              <a href="<?=base_url()?>editar_departamento/<?=$departamento['cod_departamento']?>"><i class="fa fa-pencil"></i></a>
+              <a href="<?=base_url()?>borrar_departamento/<?=$departamento['cod_departamento']?>" onclick="var result = confirm('¿Seguro que desea borrar este registro?\nEsto no se podrá revertir.'); if (result==true) { return true; } return false;"><i class="fa fa-trash-o"></i></a>
+          </td>
+        </tr>
+        <?php
+      }
+    ?>    
+  </tbody>
+</table>
+<?php
+}
+?>
+<!--LLAMAMOS EL PIE DE PAGINA-->
+<?php
+$this->load->view('administrador/pie');
+?>
